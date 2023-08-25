cd52d62aa5f123c4875c095fdcc09181bb45c269
hyejin beck <beckhye@gmail.com>
2023-08-25 14:39:50 +0900

    comment의 delete구현완료

--- articles/urls.py
+++ articles/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
 
     path('create/', views.create, name='create'),
 
-    path('<int:article_id>', views.comment_create, name='comment_create'),
+    path('<int:article_id>/comments/create', views.comment_create, name='comment_create'),
       # articles / artices_id / comments/ create 
       # articles의 해당하는 번호(상세페이지)의 댓글을 달아주세요.
+
+    path('<int:article_id>/comments/<int:id>/delete/', views.comment_delete, name='comment_delete'),
+    # -> articles/10/comments/5/delete/
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render , redirect
-from .models import Article
+from .models import Article, Comment   # Comment 추가 
 from .forms import ArticleForm, CommentForm # , CommentForm 추가 
 
 # Create your views here.
@@ -34,9 +34,22 @@ def detail(request,id):
     article = Article.objects.get(id=id)
     comment_form = CommentForm()    # 추가 
 
+    # comment목록 (1번게시글에 있는 comment만 뽑아야합니다)
+    # 첫번째 방법 
+    # comment_list = Comment.objects.filter(article=article)
+    # 이거 안할려면, context 에 있는 comment_list 도 활중시켜야함 
+    # .filter() 는 한 번 거른다.
+
+    # 두번째 방법 (권유)
+    # comment_list = article.comment_set.all()
+
+    # 세번째 방법 (적극권장))
+    # detail.html코드에서 article.comment_set.all 로 사용 
+
     context = {
         'article': article, 
-        'comment_form': comment_form, # 추가 
+        'comment_form': comment_form, 
+        #'comment_list' : comment_list, # 추가
     }
     
     return render(request, 'detail.html', context)
@@ -52,15 +65,28 @@ def comment_create(request, article_id):
         comment = comment_form.save(commit=False)
         # 어떤 데이터가 필요하다? 그럼 잠깐 저장 멈춰 
         
+        # 첫번째 방법 
         # article_id를 기준으로 article obj를 가져와서 
+        # 이 과정은 DB로 접근해서(SQL) 가져오는 방법이다. 
+        # DB로 접근해서 가져오기때문에 다소 시간이 소요될 수 있다. 
         article = Article.objects.get(id=article_id)
-
         # article 컬럼에 추가한다. 
         comment.article = article 
+
+        # 두번째 방법 
+        # 사이트 속도 저하를 막기 위해 숫자 값 하나로 저장하는 방법이다.
+        # comment.article_id = article_id 
+
 
         # 그리고 저장한다. 
         comment.save()
 
         return redirect('articles:detail', id=article_id)
 
+def comment_delete(request, article_id, id): 
+    comment = Comment.objects.get(id=id)
+
+    comment.delete()
+
+    return redirect('articles:detail', id=article_id)
 
