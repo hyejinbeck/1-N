13b4d0f2e3d32fc17c7bceec43ba1b83bae3a1f3
hyejin beck <beckhye@gmail.com>
2023-08-25 11:07:08 +0900

    create기능 구현완료

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 def index(request): 
@@ -10,3 +11,25 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+def create(request): 
+    if request.method == 'POST': # post요청일때 
+        form = ArticleForm(request.POST)   # 프론트엔드에 검증
+        if form.is_valid():           #통과되면 백엔드에 재검증
+            form.save()            # 다 통과된것만 저장 
+
+            return redirect('articles:index') # 일단 index로 보내보자.
+
+    else:                     # get요청일때  
+        form = ArticleForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'form.html', context)
+
+
+
+
+
